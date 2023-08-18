f711bcb4a709bb3d2963886290c90b31bffd3f6d
Nam9396 <nguyenthanhnam9396@gmail.com>
2023-08-18 09:02:12 +0700

    set sameSite to none

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -17,7 +17,7 @@ const generateToken = (res, user) => {
   } 
   res.cookie('jwt', token, { 
     httpOnly: true, 
-    sameSite: 'strict', 
+    sameSite: 'none', 
     secure: process.env.NODE_ENV !== 'development',
     maxAge: 30 * 24 * 60 * 60 * 1000 ,
   })
